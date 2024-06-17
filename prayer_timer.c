51011060382ddaaf49e1d8599ec9f2027342cdd0
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-06-17 15:09:19 +0300

    Create prayer_timer.c
    The module code which must be in slstatus/components/ and its declaration in slstatus.h and must modify the Makefile

---
+++ prayer_timer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../slstatus.h"
+#include "../util.h"
+
+void SecondsToString(int seconds, char *buffer, int buffer_size) {
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int secs = seconds % 60;
+    
+    snprintf(buffer, buffer_size, "%02d:%02d:%02d", hours, minutes, secs);
+}
+
+const char *
+prayer_timer(const char* path){
+    FILE* f = fopen(path, "r");
+    char* arr[6] = {"Fajr ", "Sunrise ", "Dhuhr ", "Asr ", "Maghrib ", "Isha "};
+    int idx = 0, rem = 0;
+    fscanf(f, "%d", &idx);
+    fscanf(f, "%d", &rem);
+    fclose(f);
+    char temp[10];
+    SecondsToString(rem, temp, 10);
+    char* str = (char*) malloc(32*sizeof(char));
+    strcpy(str, arr[idx]);
+    strcat(str, temp);
+    return str;
+}
