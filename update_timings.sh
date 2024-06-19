4b10616a19dd726e99bb4a5ea9745cfaf2a909ec
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-06-19 07:57:20 +0300

    Add files via upload

--- update_timings.sh
+++ update_timings.sh
@@ -26,6 +26,7 @@ fi
 if ping -q -c 1 -W 1 8.8.8.8 > /dev/null
 then
 curl -L "http://api.aladhan.com/v1/timingsByCity?city=$CI&country=$CN&method=$METHOD" -H "Accept: application/json" | jq > /home/abdalrahman/.src/Islamic-Prayer-Timings/timings.json
+jq '.data.timings' /home/abdalrahman/.src/Islamic-Prayer-Timings/timings.json > /home/abdalrahman/.src/Islamic-Prayer-Timings/table.txt
 
 g++ -o /home/abdalrahman/.src/Islamic-Prayer-Timings/up.o /home/abdalrahman/.src/Islamic-Prayer-Timings/main.cpp
 /home/abdalrahman/.src/Islamic-Prayer-Timings/up.o
